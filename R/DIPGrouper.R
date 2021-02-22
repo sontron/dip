ae1aabac21acba85db0e21f9b3b507332baefdce
sontron <sontron@foxmail.com>
2021-02-22 11:40:06 +0800

    oops

--- R/DIPGrouper.R
+++ R/DIPGrouper.R
@@ -36,19 +36,32 @@ DIPGrouper<-function(data,lstrules=lstRules){
   CCExcl(data=data,ccEcl=CCEclCHS) %>% CCStat2(data=.)->dt
   as.data.frame(dt)->dt
   
-  
-  dt$subPDX<-substr(dt$PDX,1,5)
+  # names(lstrules)->icds
+  # icds[which(stri_detect_fixed(icds,'x'))]->icdX
+  # paste('(',paste(substr(icdX,1,3),collapse='|'),')',sep='')->icdXReg
+  # 
+  # dt$subPDX<-substr(dt$PDX,1,5)
+  # 
+  # ifelse(stri_detect_regex(dt$subPDX,icdXReg),paste0(substr(dt$subPDX,1,4),'x'),dt$subPDX)->dt$subPDX
   
   dt$PROCs<-apply(dt[,paste0('PROC',1:8)],1,function(i)paste(i,collapse=';'))
   
   t(sapply(1:nrow(dt),function(i){
-    if(is.element(dt$subPDX[i],names(lstrules))){
-      lstrules[dt$subPDX[i]]->lstrulesi
+    # if(is.element(dt$subPDX[i],names(lstrules))){  ## 注销
+      if(any(stri_startswith_fixed(dt$PDX[i],names(lstrules)))){  ## 更换成开头字符判断
+      
+      # lstrules[dt$subPDX[i]]->lstrulesi  ## 注销
+        lstrules[which(stri_startswith_fixed(dt$PDX[i],names(lstrules)))]->lstrulesi
       sapply(1:length(lstrulesi[[1]]),function(j){
         all(stri_detect_regex(dt$PROCs[i],lstrulesi[[1]][[j]][['PROC']]),na.rm=T)
       })->indj
-      names(lstrulesi[[1]])[which(indj)[1]]->dipcode
-      setdiff(unlist(lstrulesi[[1]][indj][[1]]$PROC),'.')->procsj
+      
+      # names(lstrulesi[[1]])[which(indj)[1]]->dipcode
+      names(lstrulesi[[1]])[which(indj)[which.max(sapply(lstrulesi[indj],length))]]->dipcode
+      
+      # setdiff(unlist(lstrulesi[[1]][indj][[1]]$PROC),'.')->procsj
+      setdiff(unlist(lstrulesi[[1]][which(indj)[which.max(sapply(lstrulesi[indj],length))]][[1]]$PROC),'.')->procsj
+      
       if(length(procsj)>0){
         paste(procsj,collapse='+')->procsjj
       } else {
@@ -64,8 +77,31 @@ DIPGrouper<-function(data,lstrules=lstRules){
   dt$DIPCode<-dipCodes[,1]
   dt$procs<-dipCodes[,2]
   dt[dt=='OutOfRange']<-NA
+  ifelse(dt$DIPCode%in%dipDel$delete,paste0(substr(dt$DIPCode,1,5),'B'),dt$DIPCode)->dt$DIPCode
   return(dt[,c(namesDt,'CCStatus','DIPCode','procs')])
 }
 
 
+#' DIPGrouper2
+#' 
+#' @export
 
+DIPGrouper2<-function(data){
+  DIPGrouper(data)->res
+  
+  subset(res,is.na(DIPCode))->res2
+  subset(res,!is.na(DIPCode))->res1
+  
+  if(nrow(res2)>0){
+    res2[,-which(names(res2)%in%c('DIPCode','procs','CCStatus','myRowID'))]->dt2
+    
+    DIPGrouper(dt2,lstrules = lstRules2)->res2New
+    if(nrow(res2)==nrow(res)){
+      return(res2New)
+    } else {
+      return(rbind(res1,res2New))
+    }
+  } else {
+    return(res)
+  }
+}
--- R/onLoad.R
+++ R/onLoad.R
@@ -3,6 +3,7 @@
   #do not use this in combination with lazydata=true
   
   utils::data(lstRules, package = pkg, envir = parent.env(environment()))
+  utils::data(lstRules2, package = pkg, envir = parent.env(environment()))
   utils::data(ADRGVarsLst, package = pkg, envir = parent.env(environment()))
   
   utils::data(CCEclCHS, package = pkg, envir = parent.env(environment()))
@@ -10,6 +11,7 @@
   utils::data(CCStatusCHS, package = pkg, envir = parent.env(environment()))
 
   utils::data(CCMat, package = pkg, envir = parent.env(environment()))
+  utils::data(dipDel, package = pkg, envir = parent.env(environment()))
   
   
 }
